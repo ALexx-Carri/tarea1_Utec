69e43b3c3e53f229881f681931e3bcc39dba70a8
sebastianfalvy <sebastian.falvy@utec.edu.pe>
2026-04-03 12:25:34 -0500

    1.1 Fix the name of the class to Tensor

--- Claseciclo3/main.cpp
+++ Claseciclo3/main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 
-class Mat20 {
+class Tensor {
     int** ptr;
     int fil, col;
 public:
-    Mat20(int filas, int cols): fil(filas), col(cols) {
+    Tensor(int filas, int cols): fil(filas), col(cols) {
         ptr = new int*[fil];
         for ( int i = 0; i < fil; i++) {
             ptr[i] = new int[col];
@@ -16,17 +17,17 @@ public:
         }
 
     }
-    Mat20 operator+(const Mat20& matriz) {
-        Mat20 out(fil:matriz.fil, col:matriz.col);
+    Tensor operator+(const Tensor& matriz) {
+        Tensor out(fil:matriz.fil, col:matriz.col);
 
     }
-    ~Mat20() {
+    ~Tensor() {
         for (int i = 0; i < fil; i++) {
             delete[] ptr[i];
         }
         delete[]ptr;
     }
-    ostream& operator<<(ostream& os, const Mat20& matriz) {
+    ostream& operator<<(ostream& os, const Tensor& matriz) {
         for ( int i = 0; i < matriz.fil; i++) {
             for (int j= 0; j < matriz.col;j++) {
                 os << matriz.ptr[i][j];
